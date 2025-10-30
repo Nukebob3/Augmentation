b7b5ef5fadf14cc89dc9d2b63f08bd4bb32661c0
Nukebob3 <nukebobmc@gmail.com>
2025-10-30 19:58:00 +1100

    1.21.9

--- src/main/java/net/nukebob/augment/Augmentation.java
+++ src/main/java/net/nukebob/augment/Augmentation.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class Augmentation implements ClientModInitializer {
@@ -16,23 +17,25 @@ public class Augmentation implements ClientModInitializer {
 	public static int opacity;
 	public static final int maxOpacity = 2;
 
+	private static final KeyBinding.Category AUGMENTATION_CATEGORY = KeyBinding.Category.create(Identifier.of(MOD_ID, "augmentation"));
+
 	@Override
 	public void onInitializeClient() {
 		keyBindingCheck = KeyBindingHelper.registerKeyBinding(new KeyBinding(
 				"key.augment.check_missing_enchants",
 				InputUtil.Type.KEYSYM,
 				GLFW.GLFW_KEY_LEFT_ALT,
-				"category.augment.augmentation"
+				AUGMENTATION_CATEGORY
 		));
 		keyBindingAll = KeyBindingHelper.registerKeyBinding(new KeyBinding(
 				"key.augment.check_all_missing_enchants",
 				InputUtil.Type.KEYSYM,
 				GLFW.GLFW_KEY_RIGHT_ALT,
-				"category.augment.augmentation"
+				AUGMENTATION_CATEGORY
 		));
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			if (InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), KeyBindingHelper.getBoundKeyOf(Augmentation.keyBindingAll).getCode())) {
+			if (InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow(), KeyBindingHelper.getBoundKeyOf(Augmentation.keyBindingAll).getCode())) {
 				if (opacity<maxOpacity) opacity++;
 			} else if (opacity>0) opacity--;
 		});
